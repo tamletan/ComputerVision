cc1c11fd0cda5aca4e03254d08c0948542a35774
Tấn Tâm Lê <tamletannk94@gmail.com>
2020-06-13 12:10:39 +0700

    'test_model'

---
+++ face_detection_testimage.py
@@ -0,0 +1,101 @@
+from facenet_pytorch import MTCNN, InceptionResnetV1
+import torch
+from torch.utils.data import DataLoader
+from torchvision import datasets
+import numpy as np
+import pandas as pd
+import multiprocessing as mp
+import os
+import cv2
+import tensorflow as tf
+
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.utils import to_categorical
+
+thread_hold = 0.6
+dataPath = 'data'
+testPath ='test/test-images'
+dir_name = []
+
+def sys_init():
+	# set gpu memory growth True
+    gpus = tf.config.experimental.list_physical_devices('GPU')
+    if gpus:
+      try:
+        for gpu in gpus:
+          tf.config.experimental.set_memory_growth(gpu, True)
+      except RuntimeError as e:
+        print(e)
+
+    global dir_name
+    os.chdir(dataPath)
+    dir_name = [n for n in os.listdir() if os.path.isdir(n) and not n.startswith('.')]
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    print('Running on device: {}'.format(device))
+
+    #Feature extraction:
+    mtcnn = MTCNN(image_size=160, margin=0, min_face_size=40,
+        thresholds=[0.6, 0.7, 0.7], factor=0.709)
+    #Crop face from image:
+    resnet = InceptionResnetV1(pretrained='vggface2').eval().to(device)
+    os.chdir('..')
+
+    model = Sequential([
+        Dense(64, activation='relu', input_shape=(512,)),
+        Dense(64, activation='relu'),
+        Dense(len(dir_name), activation='softmax'),
+    ])
+
+    # Load the model's saved weights.
+    model.load_weights('model.h5')
+    return mtcnn,resnet,device,model
+
+def pred_image(mtcnn, resnet, device, model):
+    list_test_folder = [testPath+"/"+n for n in os.listdir(testPath) if not n.startswith('.')]
+    list_test_images = []
+    for i in list_test_folder:
+    	images_in_folder = [i+"/"+n for n in os.listdir(i) if not n.startswith('.')]
+    	list_test_images.extend(images_in_folder)
+    test_images =[]
+
+    for image in list_test_images:   
+        im = cv2.imread(image)
+        if isinstance(im, type(None)):
+            print('[Error]: ',image)
+            continue
+        im_crop = mtcnn(im)
+        if isinstance(im_crop, type(None)):
+            print('[Error]: ',image)
+            continue
+        im_crop = im_crop.to(device)
+        im_extract = resnet(im_crop.unsqueeze(0))
+        im_extract = im_extract.detach().cpu().numpy()
+        im_extract = im_extract.reshape(512)
+        test_images.append(im_extract)
+
+    test_images = np.asarray(test_images)
+
+    #predict the images:
+    predictions = model.predict(test_images[:])
+    resProb = np.amax(predictions, axis=1)
+    res = np.argmax(predictions, axis=1)
+    # print(res)
+    # print(resProb)
+    corr_count = 0
+
+    for i in range(len(res)):
+        if(resProb[i] >= thread_hold):
+            print(list_test_images[i], ":", "res: ", dir_name[res[i]], ":", "prob: ", resProb[i])
+            if list_test_images[i].split('/')[-2] == dir_name[res[i]]:
+                corr_count += 1
+        else:
+            print(list_test_images[i], ":", "Unknown")
+
+    print('ListImageFolder: ', len(list_test_images))
+    print('Correct Acc: {:.0%}'.format(corr_count/len(list_test_images)))
+
+if __name__ == '__main__':
+	np.set_printoptions(precision=3, suppress=True)
+	mtcnn, resnet, device, model = sys_init()
+	pred_image(mtcnn,resnet,device, model)
---
+++ face_detection_testvideo.py
@@ -0,0 +1,121 @@
+from facenet_pytorch import MTCNN, InceptionResnetV1
+import torch
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+import numpy as np
+import pandas as pd
+import multiprocessing as mp
+import os
+import cv2
+import tensorflow as tf
+import time
+
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.utils import to_categorical
+
+thread_hold = 0.6
+dataPath = 'data'
+dir_name = []
+
+def sys_init():
+    # set gpu memory growth True
+    gpus = tf.config.experimental.list_physical_devices('GPU')
+    if gpus:
+      try:
+        for gpu in gpus:
+          tf.config.experimental.set_memory_growth(gpu, True)
+      except RuntimeError as e:
+        print(e)
+
+    global dir_name
+    os.chdir(dataPath)
+    dir_name = [n for n in os.listdir() if os.path.isdir(n) and not n.startswith('.')]
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    print('Running on device: {}'.format(device))
+
+    #Feature extraction:
+    mtcnn = MTCNN(image_size=160, margin=0, min_face_size=40, post_process=False, 
+        thresholds=[0.6, 0.7, 0.7], factor=0.709, keep_all=True, device=device)
+
+    #Crop face from image:
+    resnet = InceptionResnetV1(pretrained='vggface2').eval().to(device)
+    os.chdir('..')
+
+    model = Sequential([
+        Dense(64, activation='relu', input_shape=(512,)),
+        Dense(64, activation='relu'),
+        Dense(len(dir_name), activation='softmax'),
+    ])
+
+    # Load the model's saved weights.
+    model.load_weights('model.h5')
+
+    return mtcnn,resnet,device,model
+
+def recognize_faces(mtcnn, resnet, device, model) :
+    cap = cv2.VideoCapture(0)
+    font = cv2.FONT_HERSHEY_SIMPLEX
+
+    start_time = time.time()
+
+    while True :
+        _, image = cap.read()
+        if cv2.waitKey(1) & 0xFF == ord('q') : # exit on q
+            break
+
+        boxes, _ = mtcnn.detect(image)
+        # print("\rTime detectbox: {:.2f}".format((time.time() - start_time)))
+
+        if not isinstance(boxes, type(None)):
+            for (x, y, w, h) in boxes:
+                x = int(x)
+                y = int(y)
+                w = int(w)
+                h = int(h)
+                cv2.rectangle(image, (x, y), (w,h), (255, 0, 0), 2)
+
+                im_crop = image[y:h, x:w]
+                try:
+                    im_crop = cv2.resize(im_crop, (160, 160), interpolation = cv2.INTER_AREA)
+                except Exception as e:
+                    print(str(e))
+                    continue
+                im_crop = ToTensor()(im_crop)
+                im_crop = im_crop.to(device)
+
+                im_extract = resnet(im_crop.unsqueeze(0))
+                # print("\rTime resnet: {:.2f}".format((time.time() - start_time)))
+
+                im_extract = im_extract.detach().cpu().numpy()
+                im_extract = im_extract.reshape(512)
+
+                predictions = model.predict(np.asarray([im_extract]))
+                resProb = np.amax(predictions)
+                res = np.argmax(predictions)
+
+                if(resProb >= thread_hold):
+                    cv2.putText(image, '{0}: {1:.1%}'.format(dir_name[res], resProb), (x+5,y-5), font, 1, (0,0,255), 2)
+                else:
+                    cv2.putText(image, "Unknown", (x+5,y-5), font, 1, (0,0,255), 2)
+
+            cv2.imshow("Face Recognizer", image)
+        else:
+            cv2.imshow("Face Recognizer", image)
+            start_time = time.time()
+            continue
+
+        # cv2.imshow("Face Recognizer", image)
+        # print("\rTime all: {:.2f}".format((time.time() - start_time)))
+        print("\rFPS: {:.2f}".format(1 / (time.time() - start_time)), end='')
+        start_time = time.time()
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    torch.cuda.init()
+    mtcnn, resnet, device, model = sys_init()
+    torch.cuda.set_device(device)
+    recognize_faces(mtcnn,resnet,device, model) 
